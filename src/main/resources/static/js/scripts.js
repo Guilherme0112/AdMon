9e13dce70b65409a4920f778d7bd5d4f78a1f8a5
Guilherme0112 <guimendesmen124@gmail.com>
2024-10-21 12:46:37 -0300

    V1.0

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector('#nav').addEventListener("click", function(){
+        document.querySelector('.hide').classList.toggle('show');
+    })
+})
